e3d0ca9bda268139f29431a7b7530911c6a79488
= <shaunt.keshishian@gmail.com>
2021-04-23 16:01:44 -0400

    CI/CD Changes 22

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set secrets via environment variables
+export TF_VAR_username=AWS_ACCESS_KEY
+export TF_VAR_password=AWS_SECRET_KEY
+
+echo AWS_ACCESS_KEY
+
 set -o errexit -o nounset
 
 cd terraform
